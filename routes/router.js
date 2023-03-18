ccc4da163d0abc24367b64fdf43bcd526f1748ab
hussein abdi <70640096+husseinyar@users.noreply.github.com>
2023-03-18 04:37:25 +0100

    fixed bug

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,7 @@
 
 import express from "express";
 const router = express.Router();
-import  login  from "../controllers/auth.js"; 
+import  login  from "../controllers/login.js"; 
 import register from "../controllers/userRegster.js"
 import { getFeedPosts, getUserPosts, likePost ,createPost } from "../controllers/posts.js";
 import {
