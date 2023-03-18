293d0cdc871e97a5703ca52faeb391b62489aa89
hussein abdi <70640096+husseinyar@users.noreply.github.com>
2023-03-18 01:38:37 +0100

    fixed bug

--- index.js
+++ index.js
@@ -11,11 +11,9 @@ import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
-import { register } from "./controllers/auth.js";
+import register from "./controllers/userRegster.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
-import User from "./models/User.js";
-import Post from "./models/Post.js";
 
 
 /* CONFIGURATIONS */
